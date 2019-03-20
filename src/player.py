3c8ae82c57b6f7d7629cf2d7ca74245a7f5f5753
tryingtokeepup <kl.hawaii@gmail.com>
2019-03-20 16:16:02 -0700

    making a quick commit, got the loop even cleaner.

--- src/player.py
+++ src/player.py
@@ -1,8 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
+# Default, no items!
+
 
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, items=None):
         self.name = name
         self.current_room = current_room
+        self.current_items = items
+
+    def getItem(self, item):
+        self.current_items.append(item)
